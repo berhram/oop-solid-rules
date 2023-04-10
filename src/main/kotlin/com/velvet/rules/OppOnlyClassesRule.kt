c044f5d0e369dd45c5320867504ea61193fad404
velvet <berhram@protonmail.com>
2023-04-11 01:37:40 +0300

    feat: new rule that forbid enums, sealed, open classes

---
+++ src/main/kotlin/com/velvet/rules/OppOnlyClassesRule.kt
@@ -0,0 +1,24 @@
+package com.velvet.rules
+
+import com.velvet.rules.core.AbstractRule
+import com.velvet.rules.core.canBeParent
+import com.velvet.rules.core.isAbstractClass
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
+
+class OppOnlyClassesRule : AbstractRule("only-oop-rules") {
+
+    override fun visitClass(
+        ktClass: KtClass,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        if (ktClass.canBeParent() && !ktClass.isAbstractClass()) {
+            emit(
+                ktClass.startOffset,
+                "Do not use enum, sealer or open class",
+                false
+            )
+        }
+    }
+}
