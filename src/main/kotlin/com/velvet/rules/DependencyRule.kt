1a8b7a38a17be1b1a3d6724681560bc07fe00f1e
velvet <berhram@protonmail.com>
2023-04-13 01:59:20 +0300

    feat: dependency rule for classes

---
+++ src/main/kotlin/com/velvet/rules/DependencyRule.kt
@@ -0,0 +1,24 @@
+package com.velvet.rules
+
+import com.velvet.rules.core.AbstractRule
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
+
+class DependencyRule : AbstractRule("dependency-rule") {
+
+    override fun visitClass(
+        ktClass: KtClass,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        ktClass.getProperties().forEach {
+            if (it.hasDelegateExpressionOrInitializer()) {
+                emit(
+                    it.startOffset,
+                    "The ${it.name} property must not be created in class",
+                    false
+                )
+            }
+        }
+    }
+}
