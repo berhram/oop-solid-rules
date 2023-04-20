71f524289bb3f76f7a0708763a8d9912c2ab9750
velvet <berhram@protonmail.com>
2023-04-20 15:43:20 +0300

    feat: encapsulation rule

--- src/main/kotlin/com/velvet/rules/EncapsulationRule.kt
+++ src/main/kotlin/com/velvet/rules/EncapsulationRule.kt
@@ -11,43 +11,42 @@ import org.jetbrains.kotlin.psi.psiUtil.visibilityModifierTypeOrDefault
 
 class EncapsulationRule : AbstractRule("encapsulation-rule") {
 
+    private val skippedAnnotation = listOf("Entity")
+
     override fun visitClass(
         ktClass: KtClass,
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
     ) {
-        val properties = ktClass.getProperties().filter { !it.hasModifier(KtTokens.OVERRIDE_KEYWORD) }
-        val parameters = ktClass.primaryConstructor?.valueParameters?.filter { it.hasValOrVar() }?.filter { !it.hasModifier(KtTokens.OVERRIDE_KEYWORD) }
-        properties.forEach {
-            val visibility = it.visibilityModifierTypeOrDefault()
-            if (visibility.isPublicOrInternal() && ktClass.canBeParent()) {
-                emit(
-                    it.startOffset,
-                    "The ${it.name} property must be private or protected",
-                    false
-                )
-            } else if (visibility.isPublicOrInternal() || (visibility.isProtected() && !ktClass.canBeParent())) {
-                emit(
-                    it.startOffset,
-                    "The ${it.name} property must be private",
-                    false
-                )
+        if (!ktClass.annotationEntries.any { annot ->
+                annot.typeReference?.text in skippedAnnotation
+            }) {
+            val properties = ktClass.getProperties().filter { !it.hasModifier(KtTokens.OVERRIDE_KEYWORD) }
+            val parameters = ktClass.primaryConstructor?.valueParameters?.filter { it.hasValOrVar() }
+                ?.filter { !it.hasModifier(KtTokens.OVERRIDE_KEYWORD) }
+            properties.forEach {
+                val visibility = it.visibilityModifierTypeOrDefault()
+                if (visibility.isPublicOrInternal() && ktClass.canBeParent()) {
+                    emit(
+                        it.startOffset, "The ${it.name} property must be private or protected", false
+                    )
+                } else if (visibility.isPublicOrInternal() || (visibility.isProtected() && !ktClass.canBeParent())) {
+                    emit(
+                        it.startOffset, "The ${it.name} property must be private", false
+                    )
+                }
             }
-        }
-        parameters?.forEach {
-            val visibility = it.visibilityModifierTypeOrDefault()
-            if (visibility.isPublicOrInternal() && ktClass.canBeParent()) {
-                emit(
-                    it.startOffset,
-                    "The constructor argument ${it.name} must be private or protected",
-                    false
-                )
-            } else if ((visibility.isPublicOrInternal()) || (visibility.isProtected() && !ktClass.canBeParent())) {
-                emit(
-                    it.startOffset,
-                    "The constructor argument ${it.name} must be private",
-                    false
-                )
+            parameters?.forEach {
+                val visibility = it.visibilityModifierTypeOrDefault()
+                if (visibility.isPublicOrInternal() && ktClass.canBeParent()) {
+                    emit(
+                        it.startOffset, "The constructor argument ${it.name} must be private or protected", false
+                    )
+                } else if ((visibility.isPublicOrInternal()) || (visibility.isProtected() && !ktClass.canBeParent())) {
+                    emit(
+                        it.startOffset, "The constructor argument ${it.name} must be private", false
+                    )
+                }
             }
         }
     }
