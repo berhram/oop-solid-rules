ab55cfe2d7097e96c71df2b0c7364365bc707cfa
velvet <berhram@protonmail.com>
2023-04-23 05:21:02 +0300

    fix: migration

--- src/main/kotlin/com/velvet/rules/OopSolidRuleSetProvider.kt
+++ src/main/kotlin/com/velvet/rules/OopSolidRuleSetProvider.kt
@@ -3,7 +3,7 @@ package com.velvet.rules
 import com.pinterest.ktlint.rule.engine.core.api.Rule
 import com.velvet.rules.core.AbstractRuleSetProvider
 
-class OopSolidRuleSetProvider : AbstractRuleSetProvider("oop-solid-rules") {
+class OopSolidRuleSetProvider : AbstractRuleSetProvider("oop") {
 
     override val rules: Set<Rule> = setOf(
         EncapsulationRule(),
